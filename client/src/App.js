c5cdc7b59939c5cefb36c05ec5b1c1fa23657b46
ShoaibDev266 <shoaibahmedgtk5@gmail.com>
2023-04-05 07:00:46 +0500

    hi

--- client/src/App.js
+++ client/src/App.js
@@ -137,8 +137,7 @@ function App() {
             />
             <button
               onClick={sendMessage}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
               Send
             </button>
           </div>
